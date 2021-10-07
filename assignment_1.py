92b19e5ce8d08091b5acc91a5d8f679a33fb77be
parul6571 <bhattparul1@gmail.com>
2021-10-07 13:11:42 +0530

    QUESTION 4

--- assignment_1.py
+++ assignment_1.py
@@ -7,8 +7,8 @@ dna_seq1=input("Please enter the DNA sequence: ")
 print("The length of DNA sequence is=",len(dna_seq1))
 
 
-# Write  a program to calculate the GC percentage of DNA sequence. Take the sequence from the user.
-
+# 2 Write  a program to calculate the GC percentage of DNA sequence. Take the sequence from the user.
+# Answer
 dna_seq2=input("please enter the DNA sequence: ")
 no_of_Gs=dna_seq2.count("G")
 no_of_Cs=dna_seq2.count("C")
@@ -27,3 +27,24 @@ print(dna_seq3_mutated)
 # dna_seq4="ATTCCGTGTGGTGTG"
 # dna_seq4_mutated=dna_seq4[:4]+"GGTTCGT"+[5:] # need to ask about string mutation is is not happening
 
+# 4. Write a program to slice the sequence in two halves. Print the result. Take the sequence from the user. 
+#answer 
+dna_seq5=input("please enter the DNA sequence: ")
+length=len(dna_seq5)
+print(dna_seq5[:int(length/2)]) #first half part
+print(dna_seq5[int(length/2):]) #second half part
+
+# 5. Print position of  the 2nd occurrence of “A”  in the sequence “ATCGATAGATACAA”.
+#answer
+
+dna_seq6="ATCGATAGATACAA"
+print("A" in dna_seq6)
+
+
+
+
+
+
+# Write a program to check which stop codons are present in the sequence  “UAAAAGGCGAGAUAAAUA” .
+# suppose that we have few restriction enzyme recognition sites ["ACGTC","CTAGT","ATGCTA"] find sites with their frequencies in the DNA sequences of  delta strain of COVID 19.
+# Write a program to reverse the second half region of a DNA sequence. "ATGATAGATAGATGATATCGATAGTA".
