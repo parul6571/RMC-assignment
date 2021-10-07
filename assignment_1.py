5d73a572aa6a3873a7e3525b5c35ff516dcf15e1
parul6571 <bhattparul1@gmail.com>
2021-10-07 14:54:40 +0530

    complete assignment, only problem with mutation in string

--- assignment_1.py
+++ assignment_1.py
@@ -22,29 +22,57 @@ dna_seq3=input("please enter the DNA sequence: ")
 dna_seq3=[dna_seq3]
 dna_seq3_mutated=dna_seq3.copy()
 dna_seq3_mutated.insert(3, "GCGGCG")
-print(dna_seq3_mutated)
+print("The mutated DNA sequence is ", dna_seq3_mutated)
 
-# dna_seq4="ATTCCGTGTGGTGTG"
-# dna_seq4_mutated=dna_seq4[:4]+"GGTTCGT"+[5:] # need to ask about string mutation is is not happening
+# dna_seq="ATTCCGTGTGGTGTG"
+# dna_seq4_mutated=dna_seq [:4]+"GGTTCGT"+[5:] # need to ask about string mutation, It is not happening
 
 # 4. Write a program to slice the sequence in two halves. Print the result. Take the sequence from the user. 
 #answer 
 dna_seq5=input("please enter the DNA sequence: ")
 length=len(dna_seq5)
-print(dna_seq5[:int(length/2)]) #first half part
-print(dna_seq5[int(length/2):]) #second half part
+print("The first half of the sequence is ", dna_seq5[:int(length/2)]) #first half part
+print("The second half of the sequence is ",dna_seq5[int(length/2):]) #second half part
+
 
 # 5. Print position of  the 2nd occurrence of “A”  in the sequence “ATCGATAGATACAA”.
-#answer
+#Answer
 
 dna_seq6="ATCGATAGATACAA"
-print("A" in dna_seq6)
+print("The DNA sequence is ", dna_seq6)
+print("The index position of 2nd occurance of A in DNA sequence is=", dna_seq6.find("A", 2))
 
 
+# 6. Write a program to check which stop codons are present in the sequence  “UAAAAGGCGAGAUAAAUA” .
+# Answer
+
+rna_seq="UAAAAGGCGAGAUAAAUA"  
+print("There are 3 stop codons are present UAA, UGA and UAG and their presence in this (UAAAAGGCGAGAUAAAUA) sequence")
+print("The presence of UAA in sequence is", "UAA" in rna_seq)
+print("The presence of UGA in sequence is", "UGA" in rna_seq)
+print("The presence of UAG in sequence is",  "UAG" in rna_seq)
 
 
+#  7. suppose that we have few restriction enzyme recognition sites ["ACGTC","CTAGT","ATGCTA"] 
+#  find sites with their frequencies in the DNA sequences of  delta strain of COVID 19.
+
+sars_cov2_DNA_sequence= "CAAATGCTAACCAACCTAGTCAACTTTCGATCTCTTGTAGATCTGTTCTCTACGTCAAACGAACTTTAAAATCTGTGTGGCTGTCACTCGGCTGCATCTAGTACGTCGCTTAGTGCACTCACGCAGTATAATTAATAACTAATTACTGTCGTTGACAGGACACGAGTAACTCGTCTATCTTCTGCAGGCTGCTTACGGTTTCGTCCGTTTTGCAGCCGATCATCAGCACATCTAGGTTTTGTCCGGGTGTGACCGAAAGGTAAGATGGAGAGCCTTGTCCCTGGTTTCAACGAGAAAACACACGTCCAACTCAGTTTGCCTGTTTTACAGGTTCGCGACATGCTAGTGCTCGTACGTGGCTATGCTATTGGAGACTCCGTGGAGGAGGTCTTATCAGAGGCCTAGTACGTCAACATCTTAAAGATGGCACTTGTGGCTTAGTAGAAGTTGAAAAAGGCGTTTTGCCTCAACTTGAACAGCCCTATGTGTTCATCAAACGTTCGGATGCTCGAACTGCACCTCATGGTCATGTTATGGTTGAGCTGGTAGCAGAACTCGAAGGCATTCAGTACGGTCGTAGTGGTGAGACACTTGGTGTCCTTGTCCCTCATGTGGGCGAAATACCAGTGGCTTACCGCAAGGTTCTTCTTCGTAAGAACGGTAATAAAGGAGCTGGTGGCCATAGTTACGGCGCCGATCTAAAGTCATTTGACTTAGGCGACGAGCTTGGCACTGATCCTTATGAAGATTTTCAAGAAAACTGGAACACTAAACATAGCAGTGGTGTTACCCGTGAACTCATGCGTGAGCTTAACGGAGGGGCATACACTCGCTATGTCGATAACAACTTCTGTGGCCCTGATGGCTACCCTCTTGAGTGCAT"
+
+print("The COVID 19 delta variant DNA sequence is", 
+sars_cov2_DNA_sequence)
+acgtc=sars_cov2_DNA_sequence.count("ACGTC")
+ctagt=sars_cov2_DNA_sequence.count("CTAGT")
+atgcta=sars_cov2_DNA_sequence.count("ATGCTA")
+
+print("The frequency of  restriction enzyme recognition sites ACGTC, CTAGT, ATGCTA in covid delta strains are", acgtc , ctagt, atgcta)
 
 
-# Write a program to check which stop codons are present in the sequence  “UAAAAGGCGAGAUAAAUA” .
-# suppose that we have few restriction enzyme recognition sites ["ACGTC","CTAGT","ATGCTA"] find sites with their frequencies in the DNA sequences of  delta strain of COVID 19.
-# Write a program to reverse the second half region of a DNA sequence. "ATGATAGATAGATGATATCGATAGTA".
+# 8 Write a program to reverse the second half region of a DNA sequence. "ATGATAGATAGATGATATCGATAGTA".
+
+#Answer 
+dna_seq7="ATGATAGATAGATGATATCGATAGTA" 
+length=len(dna_seq7)
+second_half=dna_seq7[int(length/2):]
+print("The DNA sequence is", dna_seq7)
+print("The second half of DNA sequence is", dna_seq7[int(length/2):])
+print("The reverse of second half is", second_half[::-1])
