d6db4d385d9b1e9d2e9253812396f334fed4bd5e
parul6571 <bhattparul1@gmail.com>
2021-10-15 23:00:26 +0530

    complete list assignment only question 4 needs to solve and  understand

--- list_assignment.py
+++ list_assignment.py
@@ -20,7 +20,50 @@ else:
 
 
 # Q2. Write a program to check which stop codons are present in the sequence  â€œUAAAAGGCGAGAUAAAUAâ€.
-# Q3. Check the presence of all the stop codons in the list ["UAA","UGC","AUAGCT","ATUA","UAG"] .
+#Answer
+rna_seq="UAAAAGGCGAGAUAAAUA"  
+print("There are 3 stop codons are present UAA, UGA and UAG and their presence in this (UAAAAGGCGAGAUAAAUA) sequence")
+print("The presence of UAA in sequence is", "UAA" in rna_seq)
+print("The presence of UGA in sequence is", "UGA" in rna_seq)
+print("The presence of UAG in sequence is",  "UAG" in rna_seq)
+
+
+
+# Q3. Check the presence of all the stop codons in the list ["UAA","UGC","AUAGCT","ATUA","UAG"].
+
+list2=["UAA","UGC","AUAGCT","ATUA","UAG"]
+stop_codon_1="UAA" # this is stop codon 1
+stop_codon_2="UGA"
+stop_codon_3="UAG"
+for stop_codon in list2:
+ if stop_codon.count(stop_codon_1)>0 :
+  print("There is", stop_codon.count(stop_codon_1), "of UAA in",  stop_codon)
+ elif stop_codon.count(stop_codon_2)>0:
+  print("There is", stop_codon.count(stop_codon_2), "of UGA in",  stop_codon)
+ elif stop_codon.count(stop_codon_3)>0:
+  print("There is", stop_codon.count(stop_codon_3), "of UAG in", stop_codon)
+ else:
+    print("There is no stop codon in", stop_codon)
+
+
 # Q4. Write a program to transcribe the sequence "ATGCTCGCGTAA"
+#Answer
+# DNA sequence is "ATGCTCGCGTAA" and transcription is this sequence into mRNA will give rise "UACGAGCGCAUU"
+list3=["ATGCTCGCGTAA"]
+list3=str(list3)
+list_a=list3.replace("A","U")
+list_b=list_a.replace("T","A")
+list_c=list_b.replace("G","C")
+list_d=list_b.replace("C","G")
+print(list_d)# need to ask how to convert this sequence. result should be "UACGAGCGCAUU" but geting UAGGAGGGGAUU.
+
+
 # Q5. Concatenate two lists of GC Values [30.5,12,54,23,84] and [12,45,54,32] 
-#     and find the maxium and minum GC Values.
+#     and find the maxium and minum GC Values. 
+list4=[30.5,12,54,23,84]
+list5=[12,45,54,32]
+list_new=list4+list5
+print(list_new)
+list_new.sort()
+print(list_new)
+print("The mimimum GC value is", list_new[0],"The maximum GC value is ", list_new[-1]," in the list", list_new)
